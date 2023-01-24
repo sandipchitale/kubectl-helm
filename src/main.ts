79b8a1635c63bc564b723548a8469012a80d1ee9
SANDIP CHITALE <sandipchitale@gmail.com>
2023-01-24 01:09:31 -0800

    Try to use latest revision when not specified.

--- src/main.ts
+++ src/main.ts
@@ -14,47 +14,71 @@ import * as child_process from 'child_process';
         && optsAndCommands._[0] === 'get'
         && optsAndCommands._[1] === 'templates') {
 
-        const releaseName = optsAndCommands._[2];
-        const releaseRevision = optsAndCommands.revision || '1';
         const namespace = optsAndCommands.namespace;
 
-        const secretName = `sh.helm.release.v1.${releaseName}.v${releaseRevision}`;
+        const releaseName = optsAndCommands._[2];
+
+        // revsion not specified, get the latest revision
+        let revision = optsAndCommands.revision; // start with default
+        if (!revision) {
+            const helmList = child_process.execSync(`helm list ${namespace ? `-n ${namespace}` : ''} -o json`, {
+                encoding: 'utf8'
+            });
+            try {
+                const helmListJSON = JSON.parse(helmList) as any[];
+                helmListJSON.forEach((release: any) => {
+                    if (release.name === releaseName) {
+                        revision = release.revision;
+                    }
+                });
+                // use default revision 1
+            } catch (e) {
+                // use default revision 1
+            }
+        }
+
+        const secretName = `sh.helm.release.v1.${releaseName}.v${revision}`;
         try {
             const secretBuffer = child_process.execSync(`kubectl get secret ${secretName} -o go-template="{{.data.release | base64decode }}" ${namespace ? `-n ${namespace}` : ''}`, {
                 encoding: 'utf8'
             });
-            zlib.gunzip(Buffer.from(secretBuffer, 'base64'), async (e, inflated) => {
-                if (e) {
-                    console.error(e);
-                    return;
-                } else {
-                    try {
-                        let templates = '';
-                        const helmGetAllJSON: any = JSON.parse(inflated.toString('utf8'));
-                        helmGetAllJSON.chart.templates.forEach((template: any) => {
-                            const templateString = Buffer.from(template.data, 'base64').toString('utf-8');
-                            templates += `\n---\n# Template: ${template.name}\n${templateString}`;
-                            template.data = templateString;
-                        });
-                        templates = templates.split('\\n').join('\n');
-                        console.log(`# Templates for release: ${releaseName} revision: ${releaseRevision} ${namespace ? ` in namespace ${namespace}` : ' in current namespace'}\n${templates}`);
-                    } catch (e) {
+            if (secretBuffer) {
+                zlib.gunzip(Buffer.from(secretBuffer, 'base64'), async (e, inflated) => {
+                    if (e) {
+                        console.error(e);
+                        return;
+                    } else {
+                        try {
+                            let templates = '';
+                            const helmGetAllJSON: any = JSON.parse(inflated.toString('utf8'));
+                            helmGetAllJSON.chart.templates.forEach((template: any) => {
+                                const templateString = Buffer.from(template.data, 'base64').toString('utf-8');
+                                templates += `\n---\n# Template: ${template.name}\n${templateString}`;
+                                template.data = templateString;
+                            });
+                            templates = templates.split('\\n').join('\n');
+                            console.log(`# Templates for release: ${releaseName} revision: ${revision} ${namespace ? ` in namespace ${namespace}` : ' in current namespace'}\n${templates}`);
+                        } catch (e) {
+                        }
                     }
-                }
-            });
+                });
+            } else {
+                console.error(`Could not find secret ${secretName}`);
+                return;
+            }
         } catch (e) {
             console.error(e);
             return;
         }
     } else {
-        if ((optsAndCommands._.length === 0)
-            || (optsAndCommands._.length === 2
-                && optsAndCommands._[0] === 'get'
-                && optsAndCommands._[1] === 'templates')) {
-            console.info(`The Kubernetes package manager custome commands:\n\nget templates RELEASE_NAME [--revision REVISION] [--namespace NAMESPACE]]\n`);
+        const getTemplatesUsage = `The Kubernetes package manager custome commands:\n\nget templates RELEASE_NAME [--revision REVISION] [--namespace NAMESPACE]]\n`;
+        if ((optsAndCommands._.length === 0)) { // no argumenst at all, show help for custom command
+            console.info(getTemplatesUsage);
+            child_process.execSync(`${['helm', ...rest].join(' ')}`, {
+                stdio: 'inherit'
+            });
+        } else if (optsAndCommands._.length === 2 && optsAndCommands._[0] === 'get' && optsAndCommands._[1] === 'templates') {
+            console.info(getTemplatesUsage);
         }
-        child_process.execSync(`${['helm', ...rest].join(' ')}`, {
-            stdio: 'inherit'
-        });
     }
 })();
